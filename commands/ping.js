404629902106b0e303f6ca4ab63f261a72072d50
realguy backup <101049350+realguystuff@users.noreply.github.com>
2022-12-01 10:59:33 +0000

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js');
-
+const { SlashCommandBuilder, Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Ping! Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		await interaction.editReply(`Websocket heartbeat: ${client.ws.ping} ms. 
+Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp} ms.
+
+We unfortunately could not fix the NaN ms.`);
 	},
 };
